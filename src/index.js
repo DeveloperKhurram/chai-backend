00315c9e75a78d06b4d5661d29acdad3c98932be
SheikhAbdullahSoft <audiobook.urdunovels@gmail.com>
2024-07-04 21:10:05 +0500

    utils code discussion

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 // require("dotenv").config({ path: "./env" });
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
   path: "./env",
 });
 
-connectDB();
+const port = process.env.PORT || 8000;
+connectDB()
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Server is connected at Port: ${port}`);
+    });
+  })
+  .catch((err) => {
+    console.log("DB connection failed", err);
+  });
 
 /*
 import express from "express";
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,22 @@
+const asyncHandler = (requestHandler) => {
+  (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+  };
+};
+
+export { asyncHandler };
+
+// const asyncHandler = () => {}
+// const asyncHandler = (func) => () => {}
+// const asyncHandler = (func) => async () => {}
+
+// const asyncHandler = (func) => async (req, res, next) => {
+//   try {
+//     await func(req, res, next);
+//   } catch (err) {
+//     res.status(err.code || 500).json({
+//       success: false,
+//       message: err.message,
+//     });
+//   }
+// };
